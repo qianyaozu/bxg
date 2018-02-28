d858639a42d84e891efdd29ff8505dda22679f64
qianyaozu <262236468@qq.com>
2018-02-28 17:58:54 +0800

    3

--- CabinetAPI/CabinetAPI/Controllers/BaseController.cs
+++ CabinetAPI/CabinetAPI/Controllers/BaseController.cs
@@ -3,11 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Results;
 
 namespace CabinetAPI.Controllers
 {
     public class BaseController : ApiController
     {
+        #region Action返回结果通用方法
+        /// <summary>
+        /// 返回成功数据
+        /// </summary>
+        /// <param name="data"></param>
+        public JsonResult<dynamic> Success(object data)
+        {
+            var result = new
+            {
+                State = 1,
+                Message = "",
+                Data = data
+            };
+            return Json<dynamic>(result);
+        }
+        /// <summary>
+        /// 返回失败数据
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="data"></param>
+        public JsonResult<dynamic> Failure(string message, object data = null)
+        {
+            var result = new
+            {
+                State = 0,
+                Message = message,
+                Data = data
+            };
+            return Json<dynamic>(result);
+        }
+        #endregion
 
         #region 读写Cookie
         public void WriteCookie(string strName, string strValue, int expires = 0)
--- CabinetAPI/CabinetData/Entities/UserEntities/CabinetEntity.cs
+++ CabinetAPI/CabinetData/Entities/UserEntities/CabinetEntity.cs
@@ -1,6 +1,8 @@
 using System;
-
 using CabinetData.Base;
+using System.Collections.Generic;
+using Dapper;
+using System.Linq;
 
 namespace CabinetData.Entities
 {
@@ -9,6 +11,31 @@ namespace CabinetData.Entities
 	/// </summary>
 	partial class Cabinet
 	{
+        public static List<Cabinet> GetAll()
+        {
+            var sql = "select * from Cabinet";
+            using (var cn = Database.GetDbConnection())
+            { 
+               return cn.Query<Cabinet>(sql).ToList();
+            }
+        }
 
-	}
+
+        public static Cabinet GetOne(int id)
+        {
+            var sql = "select * from Cabinet where ID=@ID";
+            using (var cn = Database.GetDbConnection())
+            {
+                return cn.Query<Cabinet>(sql,new { ID=id}).FirstOrDefault();
+            }
+        }
+        public static Cabinet GetOne(string mac)
+        {
+            var sql = "select * from Cabinet where AndroidMac=@Mac";
+            using (var cn = Database.GetDbConnection())
+            {
+                return cn.Query<Cabinet>(sql, new { Mac = mac }).FirstOrDefault();
+            }
+        }
+    }
 }
